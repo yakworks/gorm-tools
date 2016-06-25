d2253d3b40ea7fe9760c4e35a76d84ccfa9876cf
sudhir nimavat <sudhir@nimavat.me>
2016-06-25 17:36:04 +0530

    upgrade plugin version to 1.0.3

--- dao-plugin/DaoGrailsPlugin.groovy
+++ dao-plugin/DaoGrailsPlugin.groovy
@@ -19,9 +19,9 @@ class DaoGrailsPlugin {
 	static final def log = Logger.getLogger(DaoGrailsPlugin)
 	
     // the plugin version
-    def version = "1.0.2"
+    def version = "1.0.3"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.7 > *"
+    def grailsVersion = "2.5.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging

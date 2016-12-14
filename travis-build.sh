7b3a02e3bbea84cd8db30a65e499e75fd325641f
Alexey Zvegintcev <alexey.zvegintcev@sysgears.com>
2016-12-14 11:14:04 +0200

    Fixed publishing to bintray

--- travis-build.sh
+++ travis-build.sh
@@ -8,10 +8,9 @@ echo "### Running publishing"
 
 if [[ $TRAVIS_BRANCH == 'grails3' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 	echo "### publishing plugin to bintray"
-	gradle bintrayUpload
+	cd ../dao-plugin && ./gradlew bintrayUpload
 
 else
-  echo "Not on master branch, so not publishing"
   echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
   echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
   echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"

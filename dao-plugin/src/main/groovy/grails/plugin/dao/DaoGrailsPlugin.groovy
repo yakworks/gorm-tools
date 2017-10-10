f219795dbc82b6b0b5b3ffef32c9cd75766df167
Joshua B <joshdev@9ci.com>
2017-10-10 00:30:58 -0500

    tabs consistent

--- dao-plugin/src/main/groovy/grails/plugin/dao/DaoGrailsPlugin.groovy
+++ dao-plugin/src/main/groovy/grails/plugin/dao/DaoGrailsPlugin.groovy
@@ -3,27 +3,27 @@ package grails.plugin.dao
 import grails.core.ArtefactHandler
 
 class DaoGrailsPlugin extends grails.plugins.Plugin {
-	def loadAfter = ['hibernate', 'datasources']
+    def loadAfter = ['hibernate', 'datasources']
 
     def watchedResources = [
-    	"file:./grails-app/dao/**/*Dao.groovy",
-    	"file:./grails-app/services/**/*Dao.groovy",
-    	"file:./grails-app/domain/**/*.groovy",
+        "file:./grails-app/dao/**/*Dao.groovy",
+        "file:./grails-app/services/**/*Dao.groovy",
+        "file:./grails-app/domain/**/*.groovy",
         "file:./plugins/*/grails-app/dao/**/*Dao.groovy",
         "file:./plugins/*/grails-app/services/**/*Dao.groovy"
     ]
 
-	List<ArtefactHandler> artefacts = DaoPluginHelper.artefacts
+    List<ArtefactHandler> artefacts = DaoPluginHelper.artefacts
 
     Closure doWithSpring() {{->
-		Closure closure = DaoPluginHelper.doWithSpring
-		closure.delegate = delegate
-		closure.call()
-	}}
+        Closure closure = DaoPluginHelper.doWithSpring
+        closure.delegate = delegate
+        closure.call()
+    }}
 
-	@Override
+    @Override
     void onChange(Map<String,Object> event) {
-		DaoPluginHelper.onChange(event, grailsApplication)
-	}
+        DaoPluginHelper.onChange(event, grailsApplication)
+    }
 
 }

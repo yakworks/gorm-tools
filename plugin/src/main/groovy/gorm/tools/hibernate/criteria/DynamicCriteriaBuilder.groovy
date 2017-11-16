eda3049de62089b65ba549480e6c5e0829655ec5
alexeyzvegintcev <alexey.zvegintcev@sysgears.com>
2017-11-16 15:32:20 +0200

    POC for detached criterias

---
+++ plugin/src/main/groovy/gorm/tools/hibernate/criteria/DynamicCriteriaBuilder.groovy
@@ -0,0 +1,36 @@
+package gorm.tools.hibernate.criteria
+
+import grails.gorm.DetachedCriteria as GrDetachedCriteria
+import org.hibernate.criterion.Criterion
+
+public class DynamicCriteriaBuilder {
+    private Class domain
+    private List<Closure> criteriaClosures
+    private GrDetachedCriteria gdc
+
+    public DynamicCriteriaBuilder(Class domain) {
+        this.domain = domain;
+        criteriaClosures = new ArrayList<Closure>(10)
+        this.gdc = new GrDetachedCriteria(domain)
+    }
+
+    GrDetachedCriteria getCriteria(key, val) {
+        switch (val.getClass()) {
+            case (String):
+                gdc = gdc.build { like(key, val + "%") }
+                break
+            case [Boolean, boolean]:
+                gdc = gdc.build { eq(key, val) }
+                break
+            default:
+                gdc = gdc.build { eq(key, val) }
+        }
+    }
+
+    public List list(Map params) {
+        params.each { k, v ->
+            getCriteria(k, v)
+        }
+        gdc.list()
+    }
+}

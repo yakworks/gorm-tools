b91ca37f87f07632d26e66747fa13a20e540a45d
sudhir nimavat <sudhir@nimavat.me>
2017-12-08 17:21:51 +0530

    Add dao persistence events specs

--- test-projects/app/grails-app/dao/daoapp/OrgDao.groovy
+++ test-projects/app/grails-app/dao/daoapp/OrgDao.groovy
@@ -9,16 +9,6 @@ import grails.gorm.transactions.Transactional
 class OrgDao extends DefaultGormDao<Org> {
     //Class domainClass = Org
 
-//	@CompileDynamic
-//	@Override
-//	Org create(Map params){
-//		if(!params.name){
-//			params.name = "default"
-//		}
-//		if (params.name){
-//			params.name += " from Dao"
-//		}
-//		super.doInsert(params)
-//	}
+
 }
 

1770046222731379f44fcef700120af68d7f14d3
Joshua B <joshdev@9ci.com>
2017-12-12 17:43:11 -0600

    Update DaoDataTest.groovy

--- plugin/src/main/groovy/gorm/tools/testing/DaoDataTest.groovy
+++ plugin/src/main/groovy/gorm/tools/testing/DaoDataTest.groovy
@@ -23,7 +23,7 @@ trait DaoDataTest implements DataTest, AutowiredTest {
             Class daoClass = DaoTestHelper.findDaoClass(domainClass)
             daoBeans = daoBeans << DaoTestHelper.registerDao(domainClass, daoClass)
         }
-        
+
         defineBeans(daoBeans << DaoTestHelper.commonBeans())
     }
 

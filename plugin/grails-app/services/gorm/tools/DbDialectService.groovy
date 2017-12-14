8893cad51b233387f0de30690049621567f3893b
sudhir nimavat <sudhir@nimavat.me>
2017-12-14 18:04:00 +0530

    #77 Add H2 support and tests for DbDialectService

--- plugin/grails-app/services/gorm/tools/DbDialectService.groovy
+++ plugin/grails-app/services/gorm/tools/DbDialectService.groovy
@@ -4,6 +4,7 @@ import grails.util.Holders
 import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.util.ClassUtils
 
 import java.sql.SQLException
 
@@ -11,10 +12,11 @@ import java.sql.SQLException
 @CompileStatic
 class DbDialectService {
 
-    private static final int UNKNOWN = 0
-    private static final int MSSQL = 1
-    private static final int MYSQL = 2
-    private static final int ORACLE = 3
+    static final int UNKNOWN = 0
+    static final int MSSQL = 1
+    static final int MYSQL = 2
+    static final int ORACLE = 3
+    static final int H2 = 4
 
     //GrailsApplication grailsApplication
     JdbcTemplate jdbcTemplate
@@ -28,11 +30,17 @@ class DbDialectService {
         // just to make the stuff below easier to read.
         if (!dialectName) dialectName = Holders.grailsApplication.config.hibernate.dialect
 
-        if (dialectName.contains("SQLServerDialect")) result = MSSQL
+        //fallback to H2 just like how Datasources plugin does. if H2 is present in classpath
+        if ((dialectName == null && ClassUtils.isPresent("org.h2.Driver", getClass().classLoader)) || dialectName.contains('H2')) result = H2
+        else if (dialectName.contains("SQLServerDialect")) result = MSSQL
         else if (dialectName.contains("MySQL5InnoDBDialect")) result = MYSQL
         else if (dialectName.contains("Oracle")) result = ORACLE
-        if (result == UNKNOWN) throw new SQLException("Unknown dialect ${dialectName} in nine.rally.DbDialectService.\n"
-            + "Please use a known dialect or make accommodation for a new dialect.")
+
+        if (result == UNKNOWN) {
+            throw new SQLException("Unknown dialect ${dialectName} in nine.rally.DbDialectService.\n"
+                    + "Please use a known dialect or make accommodation for a new dialect.")
+        }
+
         return result
     }
 
@@ -46,6 +54,7 @@ class DbDialectService {
             case MSSQL: date = "getdate()"; break
             case MYSQL: date = "now()"; break
             case ORACLE: date = "SYSDATE"; break
+            case H2: date = "CURRENT_DATE()"; break
             default: date = "now()"
         }
         date
@@ -69,6 +78,7 @@ class DbDialectService {
             case MSSQL: concat = "+"; break
             case MYSQL: concat = "+"; break
             case ORACLE: concat = "||"; break
+            case H2: concat = "||"; break
             default: concat = "+"
         }
         concat
@@ -103,6 +113,7 @@ class DbDialectService {
             case MSSQL: dialectName = "dialect_mssql"; break
             case MYSQL: dialectName = "dialect_mysql"; break
             case ORACLE: dialectName = "dialect_oracle"; break
+            case H2: dialectName = "dialect_h2"; break
             default: dialectName = "dialect_mysql"
         }
         dialectName
@@ -147,12 +158,10 @@ class DbDialectService {
     static Map getGlobalVariables() {
         Map result = [:]
         int dialect = setupDialect()
-        if (dialect == MYSQL) {
+        if (dialect == MYSQL || dialect == H2 || dialect == ORACLE) {
             result.concat = "FN9_CONCAT"
         } else if (dialect == MSSQL) {
             result.concat = "dbo.FN9_CONCAT"
-        } else if (dialect == ORACLE) {
-            result.concat = "FN9_CONCAT"
         }
 
         return result
@@ -164,6 +173,10 @@ class DbDialectService {
 
     boolean isMsSql() {
         return dialect == MSSQL
+    }
+
+    boolean isH2() {
+        return dialect == H2
     }
 
 }
---
+++ plugin/src/test/groovy/gorm/tools/DbDialectServiceSpec.groovy
@@ -0,0 +1,220 @@
+package gorm.tools
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class DbDialectServiceSpec extends Specification implements ServiceUnitTest<DbDialectService> {
+
+    void setup() {
+        service.dialectName = null
+    }
+
+    void "test getDialect"() {
+        when: "dialect is null"
+        config.hibernate.dialect = null
+        int dialect = service.getDialect()
+
+        then:
+        dialect == DbDialectService.H2
+        service.dialectName == "dialect_h2"
+        service.isH2() == true
+
+        when: "mysql"
+        config.hibernate.dialect = 'MySQL5InnoDBDialect'
+        dialect = service.getDialect()
+
+        then:
+        dialect == DbDialectService.MYSQL
+        service.dialectName == "dialect_mysql"
+        service.isMySql() == true
+
+        when: "sql server"
+        service.dialectName = null
+        config.hibernate.dialect = 'SQLServerDialect'
+        dialect = service.getDialect()
+
+        then:
+        dialect == DbDialectService.MSSQL
+        service.dialectName == "dialect_mssql"
+        service.isMsSql() == true
+
+    }
+
+    void "test getCurrentDate"() {
+        when: "dialect is H2"
+        config.hibernate.dialect = 'H2Dialect'
+        String date = service.currentDate
+
+        then:
+        date == "CURRENT_DATE()"
+
+        when: "mysql"
+        service.dialectName = null
+        config.hibernate.dialect = 'MySQL5InnoDBDialect'
+        date = service.currentDate
+
+        then:
+        date == "now()"
+
+        when: "sql server"
+        service.dialectName = null
+        config.hibernate.dialect = 'SQLServerDialect'
+        date = service.currentDate
+
+        then:
+        date == "getdate()"
+    }
+
+    void "test getIfNull"() {
+        when: "dialect is H2"
+        config.hibernate.dialect = 'H2Dialect'
+        String function = service.ifNull
+
+        then:
+        function == "ifnull"
+
+        when: "mysql"
+        service.dialectName = null
+        config.hibernate.dialect = 'MySQL5InnoDBDialect'
+        function = service.ifNull
+
+        then:
+        function == "ifnull"
+
+        when: "sql server"
+        service.dialectName = null
+        config.hibernate.dialect = 'SQLServerDialect'
+        function = service.ifNull
+
+        then:
+        function == "isnull"
+    }
+
+    void "test getConcat"() {
+        when: "dialect is H2"
+        config.hibernate.dialect = 'H2Dialect'
+        String function = service.concat
+
+        then:
+        function == "||"
+
+        when: "mysql"
+        service.dialectName = null
+        config.hibernate.dialect = 'MySQL5InnoDBDialect'
+        function = service.concat
+
+        then:
+        function == "+"
+
+        when: "sql server"
+        service.dialectName = null
+        config.hibernate.dialect = 'SQLServerDialect'
+        function = service.concat
+
+        then:
+        function == "+"
+    }
+
+
+    void "test getCharFn"() {
+        when: "dialect is H2"
+        config.hibernate.dialect = 'H2Dialect'
+        String function = service.charFn
+
+        then:
+        function == "CHAR"
+
+        when: "mysql"
+        service.dialectName = null
+        config.hibernate.dialect = 'MySQL5InnoDBDialect'
+        function = service.charFn
+
+        then:
+        function == "CHAR"
+
+        when: "sql server"
+        service.dialectName = null
+        config.hibernate.dialect = 'SQLServerDialect'
+        function = service.charFn
+
+        then:
+        function == "CHAR"
+
+        when: "Oracle"
+        service.dialectName = null
+        config.hibernate.dialect = 'OracleDailect'
+        function = service.charFn
+
+        then:
+        function == "CHR"
+    }
+
+
+    void "test getSubstringFn"() {
+        when: "dialect is H2"
+        config.hibernate.dialect = 'H2Dialect'
+        String function = service.substringFn
+
+        then:
+        function == "SUBSTRING"
+
+        when: "mysql"
+        service.dialectName = null
+        config.hibernate.dialect = 'MySQL5InnoDBDialect'
+        function = service.substringFn
+
+        then:
+        function == "SUBSTRING"
+
+        when: "sql server"
+        service.dialectName = null
+        config.hibernate.dialect = 'SQLServerDialect'
+        function = service.substringFn
+
+        then:
+        function == "SUBSTRING"
+
+        when: "Oracle"
+        service.dialectName = null
+        config.hibernate.dialect = 'OracleDailect'
+        function = service.substringFn
+
+        then:
+        function == "SUBSTR"
+    }
+
+
+    void "test getGlobalVariables"() {
+        when: "dialect is H2"
+        config.hibernate.dialect = 'H2Dialect'
+        String function = DbDialectService.globalVariables.concat
+
+        then:
+        function == "FN9_CONCAT"
+
+        when: "mysql"
+        service.dialectName = null
+        config.hibernate.dialect = 'MySQL5InnoDBDialect'
+        function = DbDialectService.globalVariables.concat
+
+        then:
+        function == "FN9_CONCAT"
+
+        when: "sql server"
+        service.dialectName = null
+        config.hibernate.dialect = 'SQLServerDialect'
+        function = DbDialectService.globalVariables.concat
+
+        then:
+        function == "dbo.FN9_CONCAT"
+
+        when: "Oracle"
+        service.dialectName = null
+        config.hibernate.dialect = 'OracleDailect'
+        function = DbDialectService.globalVariables.concat
+
+        then:
+        function == "FN9_CONCAT"
+    }
+
+}

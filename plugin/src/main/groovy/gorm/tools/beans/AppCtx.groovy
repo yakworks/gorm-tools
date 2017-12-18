363afd7e05269631a5b08d076fec33ef34a2b80a
Joshua B <joshdev@9ci.com>
2017-12-17 20:49:02 -0600

    new AppCtx

---
+++ plugin/src/main/groovy/gorm/tools/beans/AppCtx.groovy
@@ -0,0 +1,72 @@
+package gorm.tools.beans
+
+import grails.config.Config
+import grails.core.GrailsApplication
+import grails.util.Holders
+import groovy.transform.CompileStatic
+import org.springframework.context.ApplicationContext
+
+/**
+ * A static that uses the Holder to get the spring ApplicationContext it beans and the GrailsApplication
+ * when in those cases where its not practical or possible to inject them (such as Traits for a persitenceEntity)
+ * Obviously it highly recommended to not use this and use DI whenever possible.
+ *
+ * @author Joshua Burnett (@basejump)
+ * @since 6.x
+ */
+@CompileStatic
+class AppCtx {
+
+    private static GrailsApplication cachedGrailsApplication
+    private static ApplicationContext cachedApplicationContext
+
+    /**
+     * @return the GrailsApplication
+     */
+    static GrailsApplication getGrails() {
+        if (!cachedGrailsApplication) {
+            cachedGrailsApplication = Holders.grailsApplication
+        }
+        return cachedGrailsApplication
+    }
+
+    /**
+     * @return the spring ApplicationContext
+     */
+    static ApplicationContext getCtx() {
+        if (!cachedApplicationContext) {
+            cachedApplicationContext = getGrails().mainContext
+        }
+        return cachedApplicationContext
+    }
+
+    /**
+     * @return the merged configs from application.yml, application.groovy, etc...
+     */
+    static Config getConfig() {
+        Holders.config
+    }
+
+    /**
+     * call the ApplicationContext.getBean
+     * @param name the bean name
+     * @return the bean in the context
+     */
+    static Object get(String name){
+        getCtx().getBean(name)
+    }
+
+    /**
+     * Preferred method as typed checked, call the ApplicationContext.getBean
+     *
+     * @param name the name of the bean to retrieve
+     * @param requiredType type the bean must match. Can be an interface or superclass
+     * of the actual class, or {@code null} for any match. For example, if the value
+     * is {@code Object.class}, this method will succeed whatever the class of the
+     * returned instance.
+     * @return the instance of the bean in the context
+     */
+    static public <T> T get(String name, Class<T> requiredType){
+        getCtx().getBean(name, requiredType)
+    }
+}

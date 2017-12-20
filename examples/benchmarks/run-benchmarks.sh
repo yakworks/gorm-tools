c618c43e7825520ee93a7f91e1c840b6352edd52
Joshua B <joshdev@9ci.com>
2017-12-19 23:56:17 -0600

    first line should get going for benchmarks

--- examples/benchmarks/run-benchmarks.sh
+++ examples/benchmarks/run-benchmarks.sh
@@ -3,22 +3,22 @@
 set -e
 gradle assemble; java -server -jar -DloadIterations=3 -Dgpars.poolsize=1 build/libs/benchmarks.war
 
-echo "##### Running benchamarks simple single thread"
-./gradlew assemble -DauditTrail.enabled=false
-java -server -jar -DloadIterations=1 -DpoolSize=1 -DauditTrailEnabled=true -DeventListenerCount=1 -DeventSubscriberCount=0 build/libs/benchmarks.war
+# echo "##### Running benchamarks simple single thread"
+# ./gradlew assemble -DauditTrail.enabled=false
+# java -server -jar -DloadIterations=1 -DpoolSize=1 -DauditTrailEnabled=true -DeventListenerCount=1 -DeventSubscriberCount=0 build/libs/benchmarks.war
 
-./gradlew assemble -DauditTrailEnabled=false; java -server -jar -DloadIterations=3 -Dgpars.poolsize=1 -DauditTrailEnabled=false build/libs/benchmarks.war
+# ./gradlew assemble -DauditTrailEnabled=false; java -server -jar -DloadIterations=3 -Dgpars.poolsize=1 -DauditTrailEnabled=false build/libs/benchmarks.war
 
-echo "##### Running benchamarks with AuditTrail"
-./gradlew clean assemble -DauditTrail.enabled=true
-java -jar build/libs/grails-gorm-benchmarks-0.1.war
+# echo "##### Running benchamarks with AuditTrail"
+# ./gradlew clean assemble -DauditTrail.enabled=true
+# java -jar build/libs/grails-gorm-benchmarks-0.1.war
 
-echo "##### Running benchamarks without AuditTrail"
+# echo "##### Running benchamarks without AuditTrail"
 #gradle clean assemble --no-daemon check --stacktrace
 #java -jar build/libs/grails-gorm-benchmarks-0.1.war
 
-echo "###### Running benchamarks with custom IdGenerator"
+# echo "###### Running benchamarks with custom IdGenerator"
 #java -Didgenerator.enabled=true -jar build/libs/grails-gorm-benchmarks-0.1.war
 
-echo "###### Running benchamarks with autowire off"
+# echo "###### Running benchamarks with autowire off"
 #java -Dautowire.enabled=false -jar build/libs/grails-gorm-benchmarks-0.1.war

b107ac011532dbdcf279c1f89a8482d20122528b
alexeyzvegintcev <alexey.zvegintcev@sysgears.com>
2017-12-25 13:35:33 +0200

    codernac cleanup (cherry picked from commit 657b4a5)

--- plugin/src/main/groovy/gorm/tools/hibernate/criteria/GormHibernateCriteriaBuilder.groovy
+++ plugin/src/main/groovy/gorm/tools/hibernate/criteria/GormHibernateCriteriaBuilder.groovy
@@ -133,7 +133,6 @@ class GormHibernateCriteriaBuilder extends HibernateCriteriaBuilder {
      * Dynamic method dispatch fail!
      */
     Object methodMissing(String name, args) {
-//		println "hibernate $name with $args"
         return super.invokeMethod(name, args)
     }
 

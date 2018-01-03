6fb39f354d3b17086206dd96d986cbc36c993b10
alexeyzvegintcev <alexey.zvegintcev@sysgears.com>
2018-01-03 17:37:12 +0200

    DomainAutoTest associations handling #90

--- plugin/src/test/groovy/testing/Project.groovy
+++ plugin/src/test/groovy/testing/Project.groovy
@@ -8,9 +8,15 @@ class Project {
 
     String name
     String description
+    boolean isActive
+    Nested nested
+    Date testDate
 
     static constraints = {
         name        nullable: false, example: 'project name'
         description nullable: true,  example: 'project description'
+        nested nullable: false
+        testDate nullable: false, example: "2017-01-01"
+        isActive nullable: false, example: 'false'
     }
 }

58f096fba5f1af7c45130b3ab03eb4e1d4b85348
alexeyzvegintcev <alexey.zvegintcev@sysgears.com>
2018-01-05 16:24:04 +0200

    Refactored DomainAutoTest #129

---
+++ plugin/src/main/groovy/gorm/tools/testing/BuildExampleHolder.groovy
@@ -0,0 +1,31 @@
+package gorm.tools.testing
+
+/**
+ * Holder for BuildExampleData of test classes that allows to build test values, some sort of cache
+ */
+class BuildExampleHolder {
+    private static Map<Class, BuildExampleData> holder = [:]
+
+    /**
+     * If holder already contains BuildExampleData for the class gets it from map, create new otherwise
+     *
+     * @param clazz domain class
+     * @return instance of BuildExampleData
+     */
+    static BuildExampleData get(Class clazz) {
+        if (!holder.containsKey(clazz)) {
+            holder[clazz] = new BuildExampleData(clazz)
+        }
+        holder[clazz]
+    }
+
+    /**
+     * Clears holder
+     */
+    static void clear() {
+        holder.clear()
+    }
+}
+
+
+

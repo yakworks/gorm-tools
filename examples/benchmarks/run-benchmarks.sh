516f8ff039ec98dfd7d83ec9117fd847e50b6723
Joshua B <joshdev@9ci.com>
2018-01-23 00:46:55 -0600

    fix sh for benchmark

--- examples/benchmarks/run-benchmarks.sh
+++ examples/benchmarks/run-benchmarks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-gradle assemble; java -server -jar -DloadIterations=3 -Dgpars.poolsize=1 build/libs/benchmarks.war
+gradle assemble; java -server -jar -DrunDataBindingBenchmark=true -DloadIterations=3 -Dgpars.poolsize=1 build/libs/benchmarks.war
 
 # echo "###### Running benchmarks with Second Level cache"
 #gradle assemble; java -server -jar -DloadIterations=3 -Dgpars.poolsize=1 -DsecondLevelCache=true build/libs/benchmarks.war

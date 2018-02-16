fff3e54396c6c09b518ef376a43e8df37909a5de
Joshua B <joshdev@9ci.com>
2018-02-16 02:44:09 -0600

    new utils for Tests

---
+++ plugin/src/main/groovy/gorm/tools/testing/TestTools.groovy
@@ -0,0 +1,47 @@
+package gorm.tools.testing
+
+import groovy.transform.CompileStatic
+import org.grails.datastore.gorm.GormEntity
+
+/**
+ * Misc utils for testing, asserting and "helpers" in spock tests
+ */
+@CompileStatic
+class TestTools {
+
+    /**
+     * Checks if the GormEntity's properties contains whats in expected
+     * calls mapContains after is collects the entities properties into a map
+     */
+    static boolean entityContains(GormEntity entity, Map expected) {
+        mapContains(entity.properties, expected)
+    }
+
+    /**
+     * Loosely test 2 maps for equality
+     * asserts more or less that every keySet in [a: 1, b: 2] exists in [a: 1, b: 2, c: 3] which is true in this example
+     * mapContains([a: 1, b: 2], [a: 1, c: 3]) returns false
+     * mapContains([a: 2, b: 2], [a: 1, b: 2]) also returns false
+     * if subset is an empty map or null returns false
+     *
+     * @param full the map to look in
+     * @param subset the subset of values to make sure are in the full
+     * @param exclude optional list of keys to exclude from the subset
+     * http://csierra.github.io/posts/2013/02/12/loosely-test-for-map-equality-using-groovy/
+     */
+    static boolean mapContains(Map full, Map subset, List<String> exclude=[]) {
+        //println "subset: $subset"
+        //println "full: $full"
+        if(!subset) return false
+        return subset.findAll{!exclude.contains(it.key)}.every {  it.value == full[it.key]}
+    }
+
+    /**
+     * makes sure the passed in object is a list, if not then it wraps it in one
+     * helpful when creating spocks data pipes
+     */
+    static List ensureList(obj){
+        obj instanceof List ? obj : [obj]
+    }
+
+}

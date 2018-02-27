314ba3064923300b95678d7d25ac2aa4da6204da
Joshua B <joshdev@9ci.com>
2018-02-27 03:58:30 -0600

    cleanup git user and groovydoc timestamps

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -13,9 +13,11 @@ then
        echo "### publishing SNAPSHOT"
       gradle gorm-tools:publish --no-daemon
   fi
-
+  echo "publishing docs"
+  git config --global user.name "9cibot"
+  git config --global user.email "9cibot@9ci.com"
   gradle gitPublishPush --no-daemon --max-workers 2
-  
+
 else
   echo "nothing to do here"
 fi

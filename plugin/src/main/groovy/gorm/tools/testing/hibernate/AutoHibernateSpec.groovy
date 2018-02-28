f208c2335da61c3a741778891d2c7846057b9b83
Joshua B <joshdev@9ci.com>
2018-02-28 01:20:24 -0600

    final check in

--- plugin/src/main/groovy/gorm/tools/testing/hibernate/AutoHibernateSpec.groovy
+++ plugin/src/main/groovy/gorm/tools/testing/hibernate/AutoHibernateSpec.groovy
@@ -1,3 +1,4 @@
+/* Copyright 2018. 9ci Inc. Licensed under the Apache License, Version 2.0 */
 package gorm.tools.testing.hibernate
 
 import gorm.tools.testing.TestDataJson
--- plugin/src/main/groovy/grails/plugin/gormtools/GormRepoEntityTraitInjector.groovy
+++ plugin/src/main/groovy/grails/plugin/gormtools/GormRepoEntityTraitInjector.groovy
@@ -1,3 +1,4 @@
+/* Copyright 2018. 9ci Inc. Licensed under the Apache License, Version 2.0 */
 package grails.plugin.gormtools
 
 import gorm.tools.repository.GormRepoEntity

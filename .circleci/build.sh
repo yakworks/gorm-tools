339136de90a5d05bed14727ce1d4fd3c88f85370
Joshua B <joshdev@9ci.com>
2018-04-23 18:06:07 -0500

    clean up version and build for git diff

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -9,6 +9,12 @@ echo CI_COMMIT_MESSAGE $CI_COMMIT_MESSAGE
 
 commitRange=$(echo "$CIRCLE_COMPARE_URL" | rev | cut -d/ -f1 | rev)
 echo commitRange $commitRange
+git diff --name-only
+
+# setup the git user, some of the gradle tasks that use gitPublishPush don't add the user info
+#git config --global user.name "9cibot"
+#git config --global user.email "9cibot@9ci.com"
+#git config --global credential.helper "store --file=~/.git-credentials"
 
 if [[ -z "$commitRange" || $(git diff --name-only $commitRange | grep --invert-match -E "(README\.md|mkdocs\.yml|docs/)") ]]; then
   echo "Testing - has changes that are not docs"
@@ -34,9 +40,6 @@ fi
 if [[ $CIRCLE_BRANCH == 'master' ]]; then
   if [[ $(git diff --name-only $commitRange | grep -E "(README\.md|mkdocs\.yml|docs/)") ]]; then
     echo "has doc changes"
-    git config --global user.name "9cibot"
-    git config --global user.email "9cibot@9ci.com"
-    git config --global credential.helper "store --file=~/.git-credentials"
     ./gradlew  gitPublishPush --no-daemon --max-workers 2
   fi
 fi

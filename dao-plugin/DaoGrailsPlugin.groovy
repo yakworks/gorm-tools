0341e836a41442a05116ee0bfdf9e0bc8f7adb0b
sudhir nimavat <sudhir@nimavat.me>
2019-06-06 20:15:53 +0530

    publish-plugin version 1.0.4 for grails 2.x

--- dao-plugin/DaoGrailsPlugin.groovy
+++ dao-plugin/DaoGrailsPlugin.groovy
@@ -8,7 +8,7 @@ class DaoGrailsPlugin {
 	static final def log = Logger.getLogger(DaoGrailsPlugin)
 	
     // the plugin version
-    def version = "1.0.4-SNAPSHOT"
+    def version = "1.0.4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.5.0 > *"
     // the other plugins this plugin depends on

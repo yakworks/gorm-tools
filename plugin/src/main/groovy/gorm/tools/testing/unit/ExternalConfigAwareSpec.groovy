fde5b1ce1158629d68800518b4b1b80c177acd59
sudhir nimavat <sudhir@nimavat.me>
2019-06-13 21:05:21 +0530

    cleanup, add comments, docs, move ExternalConfigLoader to separate file

--- plugin/src/main/groovy/gorm/tools/testing/unit/ExternalConfigAwareSpec.groovy
+++ plugin/src/main/groovy/gorm/tools/testing/unit/ExternalConfigAwareSpec.groovy
@@ -4,18 +4,7 @@
 */
 package gorm.tools.testing.unit
 
-import java.lang.reflect.Constructor
-
 import groovy.transform.CompileDynamic
-
-import org.springframework.beans.BeansException
-import org.springframework.boot.SpringApplication
-import org.springframework.context.ApplicationContext
-import org.springframework.context.ApplicationContextAware
-import org.springframework.core.Ordered
-import org.springframework.core.annotation.Order
-
-import grails.boot.config.GrailsApplicationPostProcessor
 
 /**
  * The trait makes it possible to load external config during unit tests.
@@ -26,34 +15,16 @@ import grails.boot.config.GrailsApplicationPostProcessor
 @CompileDynamic
 trait ExternalConfigAwareSpec {
 
+    /**
+     * externalConfigLoader is defined using doWithSpring so that it runs during Grails application lifecycle
+     * and makes the config available to GrailsConfigurationAware etc.
+     *
+     * If the test defines its own doWithSpring - it needs to call ExternalConfigAwareSpec.super.doWithSpring
+     *
+     */
     Closure doWithSpring() {
         return { ->
             externalConfigLoader(ExternalConfigLoader)
         }
     }
-}
-
-@Order(Ordered.HIGHEST_PRECEDENCE)
-class ExternalConfigLoader implements ApplicationContextAware {
-
-    /**
-     * Calls ExternalConfigRunListener from the plugin if installed which adds the property sources for external config
-     * and then grailsApplicationPostProcessor.loadApplicationConfig
-     */
-    @Override
-    @SuppressWarnings(['ClassForName', 'EmptyCatchBlock'])
-    void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        GrailsApplicationPostProcessor postProcessor = applicationContext.getBean('grailsApplicationPostProcessor')
-
-        try {
-            //run ExternalConfigRunListener it will add all the property sources to environment
-            Constructor c = Class.forName("grails.plugin.externalconfig.ExternalConfigRunListener").getConstructor(SpringApplication, String[].class)
-            Object listener = c.newInstance(null, null)
-            listener.environmentPrepared(applicationContext.environment)
-            postProcessor.loadApplicationConfig()
-        }catch (ClassNotFoundException e) {
-            //external config plugin is not installed
-        }
-    }
-
 }

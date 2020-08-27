078514ca311d123d062d7aa945f2c83eb443fb6d
Joshua B <joshdev@9ci.com>
2020-08-27 11:56:39 -0600

    new build config for testing

--- build.sh
+++ build.sh
@@ -1,37 +1,45 @@
 #!/usr/bin/env bash
 # --------------------------------------------
-# Variables and helper functions for the build and makefile
-# done in bash so it can be more flexible.
-# To show the BUILD_VARS run `./build.sh logVars <env> <dbms>`
-# substituting <evn> with test,dev, seed and <dbms> with sqlserver or mysql
+# main build script for releasing and helpers
 # --------------------------------------------
 # if build/bin does not exists then clone the bin scripts
 [ ! -e build/bin ] && git clone https://github.com/yakworks/bin.git build/bin --single-branch --depth 1;
-# user.env overrides
+# user.env overrides for local dev, not to be checked in
 [[ -f user.env ]] && source user.env
-# import build_functions.sh which has the setVar, dbEnvInit, createEnvFile, etc functions
+set -e
+# import build_functions.sh which has the the other source imports for functions
 source build/bin/build_functions.sh
-# create variables from yaml
-source build/bin/yaml
-# parse_yaml gradle/build.yml && echo
-create_yml_variables gradle/build.yml
-echo "github_fullName ${github_fullName}"
-# source the version
-source version.properties
-# call setVersion
-setVersion $version
-[ "$snapshot" == "true" ] && VERSNAP="-SNAPSHOT"
-#should match whats in settings.gradle
-PROJECT_NAME='gorm-tools'
 
-source build/bin/github_pages
-source build/bin/docmark
+# create variables from the yaml file
+create_yml_variables gradle/build.yml
+[ "$git_releasableBranchRegex" ] && RELEASABLE_BRANCHES=$git_releasableBranchRegex
+
+# PROJECT_NAME='gorm-tools'
+CHANGELOG_NAME="docs/release-notes.md"
+
+# cats key files into a cache-checksum.tmp file for circle to use as key
+# change this based on how project is structured
+function catKeyFiles {
+  cat gradle.properties build.gradle plugin/build.gradle examples/restify/build.gradle examples/app-domains/build.gradle > cache-checksum.tmp
+}
+
+function compile {
+  # Downloads Dependencies
+  ./gradlew resolveConfigurations
+  ./gradlew classes
+  ./gradlew testClasses
+  ./gradlew integrationTestClasses
+}
+
+function check {
+  ./gradlew check --max-workers=2
+}
 
 # helper/debug function ex: `build.sh logVars test sqlserver`
 function logVars {
-  initEnv ${1:-dev} ${2:-mysql}
+  # initEnv ${1:-dev} ${2:-mysql}
   for varName in $BUILD_VARS; do
-      echo "$varName = ${!varName}"
+    echo "$varName = ${!varName}"
   done
 }
 

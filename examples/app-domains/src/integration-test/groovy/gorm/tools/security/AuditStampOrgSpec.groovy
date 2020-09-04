e66730112f3d39d577c2f4648bde1c5266efabfc
Joshua B <joshdev@9ci.com>
2020-09-04 16:01:26 -0600

    stamp int test

---
+++ examples/app-domains/src/integration-test/groovy/gorm/tools/security/AuditStampOrgSpec.groovy
@@ -0,0 +1,41 @@
+package gorm.tools.security
+
+import gorm.tools.security.testing.SecuritySpecHelper
+import gorm.tools.testing.integration.DataIntegrationTest
+import grails.testing.mixin.integration.Integration
+import grails.transaction.Rollback
+import spock.lang.Specification
+import yakworks.taskify.domain.Org
+import org.apache.commons.lang.time.DateUtils
+
+@Integration
+@Rollback
+class AuditStampOrgSpec extends Specification implements DataIntegrationTest, SecuritySpecHelper {
+
+    def "test create"(){
+        when:
+        Long id = Org.create([num:'123', name:"Wyatt Oil"]).id
+        flushAndClear()
+
+        then:
+        def o = Org.get(id)
+        o.num == '123'
+        o.createdDate
+        o.createdBy == 1
+        o.editedDate
+        o.editedBy == 1
+        DateUtils.isSameInstant(o.createdDate, o.editedDate)
+
+        when: 'its edited then edited should be updated'
+        sleep(500)
+        o.num = '999'
+        o.persist(flush:true)
+
+        then:
+        o.refresh()
+        o.num == '999'
+        o.createdDate < o.editedDate
+        !DateUtils.isSameInstant(o.createdDate, o.editedDate)
+        o.editedBy == 1
+    }
+}

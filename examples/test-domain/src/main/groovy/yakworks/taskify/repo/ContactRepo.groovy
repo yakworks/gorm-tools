6ee527da6f6666d0f94f5781e14e8d118cc20c1a
Joshua B <joshdev@9ci.com>
2021-01-07 18:49:16 -0700

    Tests gorm repository fix (#283)
    fix unit tests setup for GormRepository thats not in same package

--- examples/test-domain/src/main/groovy/yakworks/taskify/repo/ContactRepo.groovy
+++ examples/test-domain/src/main/groovy/yakworks/taskify/repo/ContactRepo.groovy
@@ -1,15 +1,21 @@
-package yakworks.taskify.domain
+package yakworks.taskify.repo
 
+import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 
 import gorm.tools.databinding.BindAction
 import gorm.tools.repository.GormRepo
+import gorm.tools.repository.GormRepository
 import gorm.tools.repository.events.BeforeBindEvent
 import gorm.tools.repository.events.RepoListener
 import grails.gorm.transactions.Transactional
+import yakworks.taskify.domain.Contact
 
+@GormRepository
+@CompileStatic
 class ContactRepo implements GormRepo<Contact> {
 
+    @CompileDynamic
     @RepoListener
     void beforeBind(Contact city, Map params, BeforeBindEvent ba) {
         String name = params.remove("name")

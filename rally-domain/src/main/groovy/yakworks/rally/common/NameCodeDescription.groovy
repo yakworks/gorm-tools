e371f5d4c61549c2203e3b008c9aecf1c9cc9312
jdabal <joanna@9ci.com>
2021-06-08 13:57:44 -0500

    Create NameCodeDescription.groovy (#307)

---
+++ rally-domain/src/main/groovy/yakworks/rally/common/NameCodeDescription.groovy
@@ -0,0 +1,22 @@
+/*
+* Copyright 2021 Yak.Works - Licensed under the Apache License, Version 2.0 (the "License")
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+*/
+package yakworks.rally.common
+
+import groovy.transform.CompileStatic
+
+@SuppressWarnings(['MethodName'])
+@CompileStatic
+trait NameCodeDescription extends NameDescription {
+
+    String code
+
+    static List qSearchIncludes = ['name', 'code', 'description'] // quick search includes
+    static List picklistIncludes = ['id', 'code'] //for picklist
+
+    static constraintsMap = [
+        code:[ description: 'Short code, alphanumeric with no special characters except dash (for space) and underscore', nullable: false,
+               blank: false, maxSize: 10]
+    ]
+}

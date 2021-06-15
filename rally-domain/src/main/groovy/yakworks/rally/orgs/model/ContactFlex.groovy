1a9fc8d754c1bc960709501ebb68e4875880f7a0
Joanna Dabal <joanna@9ci.com>
2021-06-15 13:32:49 -0500

    use FlexTrait

--- rally-domain/src/main/groovy/yakworks/rally/orgs/model/ContactFlex.groovy
+++ rally-domain/src/main/groovy/yakworks/rally/orgs/model/ContactFlex.groovy
@@ -4,7 +4,7 @@
 */
 package yakworks.rally.orgs.model
 
-import java.time.LocalDateTime
+import yakworks.rally.common.FlexTrait
 
 import gorm.tools.audit.AuditStamp
 import gorm.tools.repository.model.RepoEntity
@@ -16,30 +16,9 @@ import yakworks.commons.transform.IdEqualsHashCode
 @AuditStamp
 @IdEqualsHashCode
 @GrailsCompileStatic
-class ContactFlex implements RepoEntity<ContactFlex>, Serializable{
+class ContactFlex implements FlexTrait, RepoEntity<ContactFlex>, Serializable{
 
     static belongsTo = [contact: Contact]
-
-    String text1
-    String text2
-    String text3
-    String text4
-    String text5
-    String text6
-    String text7
-    String text8
-    String text9
-    String text10
-    LocalDateTime date1
-    LocalDateTime date2
-    LocalDateTime date3
-    LocalDateTime date4
-    BigDecimal num1
-    BigDecimal num2
-    BigDecimal num3
-    BigDecimal num4
-    BigDecimal num5
-    BigDecimal num6
 
     static mapping = {
         id generator: 'foreign', params: [property: 'contact']
--- rally-domain/src/main/groovy/yakworks/rally/orgs/model/OrgFlex.groovy
+++ rally-domain/src/main/groovy/yakworks/rally/orgs/model/OrgFlex.groovy
@@ -4,6 +4,8 @@
 */
 package yakworks.rally.orgs.model
 
+import yakworks.rally.common.FlexTrait
+
 import java.time.LocalDateTime
 
 import gorm.tools.audit.AuditStamp
@@ -16,29 +18,8 @@ import yakworks.commons.transform.IdEqualsHashCode
 @AuditStamp
 @IdEqualsHashCode
 @GrailsCompileStatic
-class OrgFlex implements RepoEntity<OrgFlex>, Serializable {
+class OrgFlex implements FlexTrait, RepoEntity<OrgFlex>, Serializable {
     static belongsTo = [Org]
-
-    String text1
-    String text2
-    String text3
-    String text4
-    String text5
-    String text6
-    String text7
-    String text8
-    String text9
-    String text10
-    LocalDateTime date1
-    LocalDateTime date2
-    LocalDateTime date3
-    LocalDateTime date4
-    BigDecimal num1
-    BigDecimal num2
-    BigDecimal num3
-    BigDecimal num4
-    BigDecimal num5
-    BigDecimal num6
 
     static mapping = {
         id generator: 'assigned'

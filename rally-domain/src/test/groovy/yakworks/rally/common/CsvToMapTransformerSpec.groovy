8ca2618f26ee175e91030d498a7cd1487ae2c31f
Sudhir Nimavat <>
2022-02-14 15:42:56 +0530

    9ci/domain9#655 Add a general purpose Csv to Map transformer

---
+++ rally-domain/src/test/groovy/yakworks/rally/common/CsvToMapTransformerSpec.groovy
@@ -0,0 +1,82 @@
+package yakworks.rally.common
+
+import gorm.tools.testing.unit.DataRepoTest
+import grails.plugin.viewtools.AppResourceLoader
+import spock.lang.IgnoreRest
+import spock.lang.Shared
+import spock.lang.Specification
+import yakworks.commons.io.FileUtil
+import yakworks.commons.util.BuildSupport
+import yakworks.gorm.testing.SecurityTest
+import yakworks.rally.attachment.AttachmentSupport
+import yakworks.rally.attachment.model.Attachment
+import yakworks.rally.attachment.model.AttachmentLink
+import yakworks.rally.attachment.model.FileData
+
+import java.nio.file.Paths
+
+class CsvToMapTransformerSpec extends Specification implements DataRepoTest, SecurityTest {
+    @Shared
+    AppResourceLoader appResourceLoader
+
+    @Shared
+    CsvToMapTransformer csvToMapTransformer
+
+    def setupSpec() {
+        defineBeans {
+            appResourceLoader(AppResourceLoader) {
+                grailsApplication = grailsApplication
+                resourcesConfigRootKey = "app.resources"
+            }
+            attachmentSupport(AttachmentSupport)
+            csvToMapTransformer(CsvToMapTransformer)
+        }
+        mockDomains(Attachment, AttachmentLink, FileData)
+    }
+
+    void "sanity checks"() {
+        expect:
+        appResourceLoader != null
+        appResourceLoader.rootLocation.exists()
+        csvToMapTransformer != null
+
+        new File("src/test/resources/csv/contact.csv").exists()
+    }
+
+    @IgnoreRest
+    void "test with zip"() {
+        setup:
+        def dataCsv =  new File("src/test/resources/csv/contact.csv")
+        File zip = FileUtil.zip("test.zip", null, dataCsv)
+
+        expect:
+        dataCsv.exists()
+        zip.exists()
+
+        when: "create attachment"
+        Map params = [name: "test.zip", sourcePath: zip.toPath()]
+        Attachment attachment = Attachment.create(params)
+
+        then:
+        noExceptionThrown()
+        attachment != null
+        attachment.id != null
+        attachment.resource.getFile().exists()
+
+        when:
+        List<Map> rows = csvToMapTransformer.process([attachmentId:attachment.id, dataFilename:"contact.csv"])
+
+        then:
+        noExceptionThrown()
+        rows != null
+        rows.size() == 3
+        rows[0].name == "name1"
+        rows[0].num == "num1"
+        rows[0]["org.source.sourceId"] == "source1"
+
+        cleanup:
+        if(zip.exists()) zip.delete()
+        if(attachment) attachment.resource.getFile()?.delete()
+    }
+
+}

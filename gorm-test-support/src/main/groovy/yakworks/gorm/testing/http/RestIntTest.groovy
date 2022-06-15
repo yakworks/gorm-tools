0432e4dd63b9a0a2477f34aa169f6275c0baf8d0
Joshua B <joshdev@9ci.com>
2022-06-15 17:08:28 -0600

    tests passing

--- gorm-test-support/src/main/groovy/yakworks/gorm/testing/http/RestIntTest.groovy
+++ gorm-test-support/src/main/groovy/yakworks/gorm/testing/http/RestIntTest.groovy
@@ -7,12 +7,6 @@ package yakworks.gorm.testing.http
 import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 
-import gorm.tools.beans.AppCtx
-import gorm.tools.testing.integration.DataIntegrationTest
-import gorm.tools.transaction.TrxService
-import grails.core.GrailsApplication
-import grails.util.GrailsMetaClassUtils
-import grails.util.GrailsWebMockUtil
 import org.codehaus.groovy.runtime.HandleMetaClass
 import org.grails.core.artefact.ControllerArtefactHandler
 import org.grails.web.servlet.mvc.GrailsWebRequest
@@ -21,6 +15,13 @@ import org.junit.Before
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.context.WebApplicationContext
 import org.springframework.web.context.request.RequestContextHolder
+
+import gorm.tools.beans.AppCtx
+import gorm.tools.testing.integration.DataIntegrationTest
+import gorm.tools.transaction.TrxService
+import grails.core.GrailsApplication
+import grails.util.GrailsMetaClassUtils
+import grails.util.GrailsWebMockUtil
 import spock.lang.Specification
 
 /**

14c5037158682b4fc5dadd68f86fe2f711bd0dd3
Joshua B <joshdev@9ci.com>
2022-08-08 11:54:02 -0600

    move to Msg.key for the static helper

--- gorm-tools-security/src/main/groovy/gorm/tools/security/PasswordValidator.groovy
+++ gorm-tools-security/src/main/groovy/gorm/tools/security/PasswordValidator.groovy
@@ -17,6 +17,7 @@ import grails.compiler.GrailsCompileStatic
 import grails.gorm.transactions.Transactional
 import yakworks.api.Result
 import yakworks.api.problem.Problem
+import yakworks.message.Msg
 import yakworks.message.MsgKey
 
 @CompileStatic
@@ -49,31 +50,31 @@ class PasswordValidator {
     Result validate(AppUser user, String pass, String passConfirm) {
         List problemKeys = [] as List<MsgKey>
         if (!pass || (pass.length() < passwordMinLength)) {
-            problemKeys << MsgKey.of("security.validation.password.minlength", [min: passwordMinLength])
+            problemKeys << Msg.key("security.validation.password.minlength", [min: passwordMinLength])
         }
 
         if (passConfirm != pass) {
-            problemKeys << MsgKey.ofCode("security.validation.password.match")
+            problemKeys << Msg.key("security.validation.password.match")
         }
 
         if (passwordMustContainLowercaseLetter && !(pass =~ /^.*[a-z].*$/)) {
-            problemKeys << MsgKey.ofCode("security.validation.password.mustcontain.lowercase")
+            problemKeys << Msg.key("security.validation.password.mustcontain.lowercase")
         }
 
         if (passwordMustContainUpperaseLetter && !(pass =~ /^.*[A-Z].*$/)) {
-            problemKeys << MsgKey.ofCode("security.validation.password.mustcontain.uppercase")
+            problemKeys << Msg.key("security.validation.password.mustcontain.uppercase")
         }
 
         if (passwordMustContainNumbers && !(pass =~ /^.*[0-9].*$/)) {
-            problemKeys << MsgKey.ofCode("security.validation.password.mustcontain.numbers")
+            problemKeys << Msg.key("security.validation.password.mustcontain.numbers")
         }
 
         if (passwordMustContainSymbols && !(pass =~ /^.*\W.*$/)) {
-            problemKeys << MsgKey.ofCode("security.validation.password.mustcontain.symbol")
+            problemKeys << Msg.key("security.validation.password.mustcontain.symbol")
         }
 
         if (passwordHistoryEnabled && passwordExistInHistory(user, pass)) {
-            problemKeys << MsgKey.of("security.validation.password.minlength", [value: passwordHistoryLength])
+            problemKeys << Msg.key("security.validation.password.minlength", [value: passwordHistoryLength])
         }
 
         if(problemKeys){

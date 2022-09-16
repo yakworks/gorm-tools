247dc6955609d44fd6462f368b15b1932c8a4bff
Joshua B <joshdev@9ci.com>
2022-09-16 08:33:27 -0600

    remove check for servlet on GrailsAppBuilder, tie to specifc versions for testing-support

--- gorm-test-support/src/main/groovy/yakworks/testing/grails/GrailsAppBuilder.groovy
+++ gorm-test-support/src/main/groovy/yakworks/testing/grails/GrailsAppBuilder.groovy
@@ -8,18 +8,9 @@ import groovy.transform.CompileStatic
 
 import org.grails.testing.GrailsApplicationBuilder
 import org.springframework.beans.factory.config.ConfigurableBeanFactory
-import org.springframework.beans.factory.support.DefaultListableBeanFactory
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
-import org.springframework.core.convert.ConversionService
 import org.springframework.core.env.ConfigurableEnvironment
-import org.springframework.core.env.Environment
-import org.springframework.core.env.PropertyResolver
-
-import grails.boot.GrailsApp
-import io.micronaut.context.ApplicationContextConfiguration
-import io.micronaut.context.DefaultApplicationContext
-import io.micronaut.spring.context.factory.MicronautBeanFactoryConfiguration
 
 /**
  * Copied in from grails-testing-support Created by jameskleeh on 5/31/17.
@@ -32,63 +23,14 @@ class GrailsAppBuilder extends GrailsApplicationBuilder {
     protected ConfigurableApplicationContext createMainContext(Object servletContext) {
         ConfigurableApplicationContext context = new AnnotationConfigApplicationContext()
 
-        if (isServletApiPresent && servletContext != null) {
-            context = new AnnotationConfigApplicationContext() //AnnotationConfigServletWebServerApplicationContext()
-        } else {
-            context = new AnnotationConfigApplicationContext()
-        }
         context.register(BasicConfiguration)
         ClassLoader applicationClassLoader = this.class.classLoader
         ConfigurableEnvironment configuredEnvironment = context.getEnvironment()
-        ApplicationContextConfiguration micronautConfiguration = new ApplicationContextConfiguration() {
-            @Override
-            List<String> getEnvironments() {
-                if (configuredEnvironment != null) {
-                    return configuredEnvironment.getActiveProfiles().toList()
-                } else {
-                    return Collections.emptyList()
-                }
-            }
-
-            @Override
-            Optional<Boolean> getDeduceEnvironments() {
-                return Optional.of(false)
-            }
-
-            @Override
-            ClassLoader getClassLoader() {
-                return applicationClassLoader
-            }
-        }
 
         ConfigurableBeanFactory beanFactory = context.getBeanFactory()
-        List beanExcludes = []
-        beanExcludes.add(ConversionService)
-        beanExcludes.add(Environment)
-        beanExcludes.add(PropertyResolver)
-        beanExcludes.add(ConfigurableEnvironment)
-        def objectMapper = io.micronaut.core.reflect.ClassUtils.forName("com.fasterxml.jackson.databind.ObjectMapper", context.getClassLoader()).orElse(null)
-        if (objectMapper != null) {
-            beanExcludes.add(objectMapper)
-        }
-        def micronautContext = new DefaultApplicationContext(micronautConfiguration);
-        micronautContext
-                .environment
-                .addPropertySource("grails-config", [(MicronautBeanFactoryConfiguration.PREFIX + ".bean-excludes"): (Object) beanExcludes])
-        micronautContext.start()
-        ConfigurableApplicationContext parentContext = micronautContext.getBean(ConfigurableApplicationContext)
-        ((DefaultListableBeanFactory) beanFactory)
-                .setAllowBeanDefinitionOverriding(true);
-        ((DefaultListableBeanFactory) beanFactory)
-                .setAllowCircularReferences(true);
-        context.setParent(
-                parentContext
-        )
-        context.addApplicationListener(new GrailsApp.MicronautShutdownListener(micronautContext))
         prepareContext(context, beanFactory)
         context.refresh()
         context.registerShutdownHook()
-
         context
     }
 

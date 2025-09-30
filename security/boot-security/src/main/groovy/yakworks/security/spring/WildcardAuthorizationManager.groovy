1c1640bb84348291d2093a6720fb0b73e903ffd4
Sudhir Nimavat <sudhir@nimavat.me>
2025-09-30 22:32:09 +0530

    Add XXX

--- security/boot-security/src/main/groovy/yakworks/security/spring/WildcardAuthorizationManager.groovy
+++ security/boot-security/src/main/groovy/yakworks/security/spring/WildcardAuthorizationManager.groovy
@@ -79,6 +79,7 @@ class WildcardAuthorizationManager implements AuthorizationManager<RequestAuthor
 
             //if its a put or delete request, and last part is number, eg PUT /api/ar/tran/1
             //then remove the last part (id) to build a permission like ar:tran:read, isntead of ar:tran:1:read
+            //XXX @SUD needs fix to handle uuid IDs
             if(op in ['update', 'delete', 'read'] && segments[-1].isNumber()) {
                 segments.removeAt(segments.size() - 1) //removes last item
             }
